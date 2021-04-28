a96d5e4deec66b8b36118f243071b41ae63babd4
Pathik Patel <pathik.s.patel@gmail.com>
2021-04-29 03:01:15 +0530

    fixed mobile screens

--- js/custom.js
+++ js/custom.js
@@ -6,97 +6,98 @@
 
 $.cssNumber.zoom = true;
 if (!("zoom" in document.body.style)) {
-	$.cssHooks.zoom = {
-		get: function(elem, computed, extra) {
-			var value = $(elem).data('zoom');
-			return value != null ? value : 1;
-		},
-		set: function(elem, value) {
-			var $elem = $(elem);
-			var size = { // without margin
-				width: $elem.outerWidth(),
-				height: $elem.outerWidth()
-			};
-			$elem.data('zoom', value);
-			if (value != 1) {
-				$elem.css({
-					transform: 'scale(' + value + ')',
-					marginLeft: (size.width * value - size.width) / 2,
-					marginRight: (size.width * value - size.width) / 2,
-					marginTop: (size.height * value - size.height) / 2,
-					marginBottom: (size.height * value - size.height) / 2
-				});
-			} else {
-				$elem.css({
-					transform: null,
-					margin: null
-				});
-			}
-		}
-	};
+    $.cssHooks.zoom = {
+        get: function(elem, computed, extra) {
+            var value = $(elem).data('zoom');
+            return value != null ? value : 1;
+        },
+        set: function(elem, value) {
+            var $elem = $(elem);
+            var size = { // without margin
+                width: $elem.outerWidth(),
+                height: $elem.outerWidth()
+            };
+            $elem.data('zoom', value);
+            if (value != 1) {
+                $elem.css({
+                    transform: 'scale(' + value + ')',
+                    marginLeft: (size.width * value - size.width) / 2,
+                    marginRight: (size.width * value - size.width) / 2,
+                    marginTop: (size.height * value - size.height) / 2,
+                    marginBottom: (size.height * value - size.height) / 2
+                });
+            } else {
+                $elem.css({
+                    transform: null,
+                    margin: null
+                });
+            }
+        }
+    };
 }
 
 
-$( document ).ready(function() {
-  var defaultWidth = 1920;
-  var currentWindowWidth = window.innerWidth;
+$(document).ready(function() {
+    var defaultWidth = 1920;
+    var currentWindowWidth = window.innerWidth;
 
-  if(currentWindowWidth > 1024 && currentWindowWidth < defaultWidth){
-	var percent = (currentWindowWidth * 100 )/defaultWidth;
-	document.body.style.zoom = Math.round(percent) + '%';
-  }
+    if (currentWindowWidth > 1024 && currentWindowWidth < defaultWidth) {
+        var percent = (currentWindowWidth * 100) / defaultWidth;
+        document.body.style.zoom = Math.round(percent) + '%';
+    }
 
-  //$(document).on("scroll", onScroll);
-  //smoothscroll
-	$('a.nav').on('click', function (e) {
-		$('a.nav').each(function () {
-			$(this).removeClass('active');
-		})
-		$(this).addClass('active');
-	});
+    //$(document).on("scroll", onScroll);
+    //smoothscroll
+    $('a.nav:not(.dropbtn)').on('click', function(e) {
+        $('a.nav').each(function() {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+    });
 
 });
 
 
 
 function onScroll(event) {
-	var scrollPos = $(document).scrollTop();
-	$('.nav').each(function () {
-		var percent = 100;
-		var defaultWidth = 1920;
-		var currentWindowWidth = window.innerWidth;
-		if (currentWindowWidth > 1024 && currentWindowWidth < defaultWidth) {
-			percent = (currentWindowWidth * 100) / defaultWidth;
-		}
-		var currLink = $(this);
-		var refElement = $(currLink.attr("href"));
-		if (refElement.position().top <= (scrollPos + (scrollPos * (1 - percent / 100) + (window.innerHeight/2)))) {
-			$('.nav').removeClass("active");
-			currLink.addClass("active");
-		}
-		else {
-			currLink.removeClass("active");
-		}
-	});
+    var scrollPos = $(document).scrollTop();
+    $('.nav').each(function() {
+        var percent = 100;
+        var defaultWidth = 1920;
+        var currentWindowWidth = window.innerWidth;
+        if (currentWindowWidth > 1024 && currentWindowWidth < defaultWidth) {
+            percent = (currentWindowWidth * 100) / defaultWidth;
+        }
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+
+        if (refElement.position().top <= (scrollPos + (scrollPos * (1 - percent / 100) + (window.innerHeight / 2)))) {
+            $('.nav').removeClass("active");
+            currLink.addClass("active");
+        } else {
+            currLink.removeClass("active");
+        }
+    });
 }
 
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function openDropdown() {
-	document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").classList.toggle("show");
+    $('.dropbtn').blur();
 }
 
 // Close the dropdown if the user clicks outside of it
-window.onclick = function (event) {
-	if (!event.target.matches('.dropbtn')) {
-		var dropdowns = document.getElementsByClassName("dropdown-content");
-		var i;
-		for (i = 0; i < dropdowns.length; i++) {
-			var openDropdown = dropdowns[i];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
